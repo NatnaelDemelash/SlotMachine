8cf67021d8d7b2b434abb4aa7100cd66428daa8e
Natnael-Demelash <natnael@gooday.io>
2022-10-25 23:21:47 +0300

    added get_ bet function and bet related validations

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from curses.ascii import isdigit
 
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 def deposit():
     '''
@@ -34,9 +36,35 @@ def get_number_of_lines():
     return lines
 
 
+def get_bet():
+    while True:
+        amount = input('What would you like to bet on each line? $ ')
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <=  amount <= MAX_BET  :
+                break
+            else:
+                print(f"Your bet must be between ${MIN_BET} - ${MAX_BET}")
+        else:
+            print("Please enter a number.")
+    
+    return amount
+
+
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance, lines)
+
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f'You dont have enough to bet that amount. your current balance is ${balance}!')
+        else:
+            break
+
+    print(f"Your bet is ${bet} in {lines} lines. Your total bet is ${total_bet}")
+
 
 main()
